5a775db77709cfecf1fe0e955340a2fa53f69ccd
aguiot-- <aguiot--@student.42.fr>
2018-11-05 18:17:40 +0100

    ex18

---
+++ ex18/ft_print_params.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aguiot-- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/05 18:09:06 by aguiot--          #+#    #+#             */
+/*   Updated: 2018/11/05 18:17:28 by aguiot--         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	if (argc == 1)
+		return (0);
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		ft_putchar('\n');
+		i++;
+	}
+}
